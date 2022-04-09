ceb02b0755fc016a925bbc26735da87ad1d2aca7
Ting Yu Wong <sandy20615@gmail.com>
2022-04-09 16:52:16 +0800

    Update index.js
    from scratch => not from "a" scratch ^^

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -3,7 +3,7 @@ import Plugin from '../components/Plugin.vue'
 export default {
     install: (app, options) => {
         app.config.globalProperties.$first = () => {
-            console.log('Tina!!! Can you believe this??? You make a plugin from a scratch!!!')
+            console.log('Make a plugin from scratch!!!')
         }
         
         app.component('Plugin', Plugin)
